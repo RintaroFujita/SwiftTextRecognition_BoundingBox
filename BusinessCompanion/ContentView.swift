8abf5e6ffbf6879f2bc2ba8ff575838564e64cca
Rintaro Fujita <rinpc356@gmail.com>
2024-10-03 14:03:08 +0900

    update

--- BusinessCompanion/ContentView.swift
+++ BusinessCompanion/ContentView.swift
@@ -5,8 +5,8 @@ import SwiftUI
 struct ContentView: View {
     @StateObject private var imageTextRecognition = ImageTextRecognition()
     
-    let directoryPath = "/Users/r/github/SwiftTextRecognition_BoundingBox/BusinessCompanion/Parking"
-    @State private var saveDirectory: String = "/Users/r/github/SwiftTextRecognitoin_MisrecognizeDatasetImage/Parking" // Change each time!
+    let directoryPath = "/Users/r/github/SwiftTextRecognition_BoundingBox/BusinessCompanion/Town_Aerial_Image2_5"
+    @State private var saveDirectory: String = "/Users/r/github/SwiftTextRecognitoin_MisrecognizeDatasetImage/Town_Aerial_Image2" // Change each time!
 
     var body: some View {
         VStack {
